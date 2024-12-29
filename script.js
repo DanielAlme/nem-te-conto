b3b541fc043519e881eeb9f6df16b15a07f8a512
DanielAlme <danielalmeida22012008@gmail.com>
2024-12-29 20:53:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-function showImage(imagemId) {
-    // Esconde todas as imagens
-    var imagens = document.querySelectorAll('.image-container img');
-    imagens.forEach(function(img) {
-        img.classList.add('hidden');
-    });
-
-    // Exibe a imagem correspondente
-    var imagem = document.getElementById(imagemId);
-    if (imagem) {
-        imagem.classList.remove('hidden');
+function showImage(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == 'none'){
+        document.getElementById(el).style.display = 'block';
+    }
+    else{
+        document.getElementById(el).style.display = 'none';
     }
 }
